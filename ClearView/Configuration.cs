04287b220b380db368727872eb001948581847e5
Chris <madmac252@protonmail.com>
2025-04-10 10:58:43 +0200

    refactor: upgrade to Dalamud.NET.Sdk and fix CS8618 warnings - Upgrade project to use Dalamud.NET.Sdk/12.0.0 - Remove Dalamud.Plugin.Bootstrap.targets - Initialize string properties in Configuration.cs - Update package references

--- ClearView/Configuration.cs
+++ ClearView/Configuration.cs
@@ -9,25 +9,25 @@ public class Configuration : IPluginConfiguration
 {
     public int Version { get; set; } = 0;
 
-    public string DisplayTypeSelf { get; set; }
-    public string DisplayTypeParty { get; set; }
-    public string DisplayTypeAlliance { get; set; }
-    public string DisplayTypeOther { get; set; }
+    public string DisplayTypeSelf { get; set; } = string.Empty;
+    public string DisplayTypeParty { get; set; } = string.Empty;
+    public string DisplayTypeAlliance { get; set; } = string.Empty;
+    public string DisplayTypeOther { get; set; } = string.Empty;
 
-    public string TitleDisplaySelf { get; set; }
-    public string TitleDisplayParty { get; set; }
-    public string TitleDisplayAlliance { get; set; }
-    public string TitleDisplayOther { get; set; }
+    public string TitleDisplaySelf { get; set; } = string.Empty;
+    public string TitleDisplayParty { get; set; } = string.Empty;
+    public string TitleDisplayAlliance { get; set; } = string.Empty;
+    public string TitleDisplayOther { get; set; } = string.Empty;
 
-    public string CombatDisplayTypeSelf { get; set; }
-    public string CombatDisplayTypeParty { get; set; }
-    public string CombatDisplayTypeAlliance { get; set; }
-    public string CombatDisplayTypeOther { get; set; }
+    public string CombatDisplayTypeSelf { get; set; } = string.Empty;
+    public string CombatDisplayTypeParty { get; set; } = string.Empty;
+    public string CombatDisplayTypeAlliance { get; set; } = string.Empty;
+    public string CombatDisplayTypeOther { get; set; } = string.Empty;
 
-    public string CombatTitleDisplaySelf { get; set; }
-    public string CombatTitleDisplayParty { get; set; }
-    public string CombatTitleDisplayAlliance { get; set; }
-    public string CombatTitleDisplayOther { get; set; }
+    public string CombatTitleDisplaySelf { get; set; } = string.Empty;
+    public string CombatTitleDisplayParty { get; set; } = string.Empty;
+    public string CombatTitleDisplayAlliance { get; set; } = string.Empty;
+    public string CombatTitleDisplayOther { get; set; } = string.Empty;
 
     // the below exist just to make saving less cumbersome
     public void Save()
